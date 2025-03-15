8013bae21fbba9636383a4da33bc2251411526a2
Meron <mbelachew26@gmail.com>
2025-03-15 14:03:58 -0400

    added cream header to projects section.

--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -1,6 +1,6 @@
 const Projects = () => {
   return (
-    <section id="projects" className="relative pt-32 pb-32 px-8 bg-[#EDE2D6]">
+    <section id="projects" className="relative pt-10 pb-32 px-8 bg-[#EDE2D6]">
       {/* Curved top edge - curved upward */}
       <div 
         className="absolute top-0 left-0 right-0 h-[100px] -translate-y-[99%] z-10"
@@ -19,15 +19,23 @@ const Projects = () => {
         }}
       ></div>
       
-      <div className="flex flex-col items-end max-w-6xl mx-auto pr-2 md:pr-4 relative z-20">
-        <h1 className="text-6xl font-acorn text-[#4A6F63] mb-20">Projects</h1>
+      <div className="flex flex-col max-w-6xl mx-auto relative z-20">
+        <h1 className="text-6xl font-acorn text-[#4A6F63] mb-20 self-end pr-4">Projects</h1>
         
-        <div className="w-full md:w-1/2 lg:w-1/3">
-          <img 
-            src="/android-phone.svg" 
-            alt="Android Phone" 
-            className="w-full h-auto"
-          />
+        <div className="flex flex-col md:flex-row items-start justify-between w-full">
+          <div className="md:w-1/2 mb-10 md:mb-0 md:pl-0 md:mt-8">
+            <h2 className="text-5xl md:text-6xl font-acorn text-[#A79687] leading-tight">
+              I work on Android<br />Mobile apps
+            </h2>
+          </div>
+          
+          <div className="md:w-1/2 lg:w-1/3 self-end">
+            <img 
+              src="/android-phone.svg" 
+              alt="Android Phone" 
+              className="w-full h-auto"
+            />
+          </div>
         </div>
       </div>
     </section>
