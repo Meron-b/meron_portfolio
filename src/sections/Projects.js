81a10454808ed39fd8057655bfbc307f920d3d82
Meron <mbelachew26@gmail.com>
2025-03-15 14:36:33 -0400

    added pink project showcase.

--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -27,6 +27,44 @@ const Projects = () => {
             <h2 className="text-5xl md:text-6xl font-acorn text-[#A79687] leading-tight">
               I work on Android<br />Mobile apps
             </h2>
+            
+            {/* Project Showcase */}
+            <div 
+              className="relative rounded-xl overflow-hidden bg-[#E0ABC5]/[0.59] p-6 mt-20"
+              style={{ width: '565px', height: '420px' }}
+            >
+              <div className="absolute top-6 right-6 text-right">
+                <span className="text-[20px] font-comic-neue text-[#4A6F63] text-lg">Dialer</span>
+                
+                <h3 className="text-[24px] font-acorn text-[#4A6F63] mt-0 text-right">
+                  DTMF Player API and Feature Integrations
+                </h3>
+              </div>
+              
+              {/* Green phone in bottom left */}
+              <div 
+                className="absolute bottom-6 left-6"
+                style={{ width: '24px', height: '24px' }}
+              >
+                <img 
+                  src="/green-mini-phone.svg" 
+                  alt="Green Phone" 
+                  className="w-full h-full"
+                />
+              </div>
+              
+              {/* Android phone half in bottom right */}
+              <div 
+                className="absolute bottom-0 right-5 transition-transform duration-300 hover:scale-105"
+                style={{ width: '437px', height: '308px' }}
+              >
+                <img 
+                  src="/android-phone-half.svg" 
+                  alt="Project Screenshot" 
+                  className="w-full h-full object-contain"
+                />
+              </div>
+            </div>
           </div>
           
           <div className="md:w-1/2 lg:w-1/3 self-end">
